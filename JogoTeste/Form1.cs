1ec34c65b45d8172416888edf4a754a1a61b8239
Vitorr-AF <vitoraugustofranca7@gmail.com>
2026-02-28 12:32:35 -0300

    Código organizado e comentado

--- JogoTeste/Form1.cs
+++ JogoTeste/Form1.cs
@@ -2,8 +2,28 @@ namespace JogoTeste
 {
     public partial class Form1 : Form
     {
+        // Definição do Form
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
+        // Funções de Centralização do form com tela cheia
+        private void Centralizar(Panel interno, Panel pai)
+        {
+            interno.Left = (pai.Width - interno.Width) / 2;
+            interno.Top = (pai.Height - interno.Height) / 2;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Centralizar(panelCentroMenu, panelMenu);
+            Centralizar(panelCentroJogo1, panelJogo1);
+        }
+
+        // Definição das classes
         private Player player;
+        List<Inimigo> inimigos = new List<Inimigo>();
 
         public class Player
         {
@@ -12,12 +32,13 @@ namespace JogoTeste
             public int EnergiaMax;
             public int EnergiaAtual;
         }
-
-        public Form1()
+        public class Inimigo
         {
-            InitializeComponent();
+            public int Vida { get; set; }
+            public string Nome { get; set; }
         }
 
+        // Botões do Menu de Inicio, e suas funções
         private void btnStartGame_Click(object sender, EventArgs e)
         {
             panelMenu.Visible = false;
@@ -42,36 +63,7 @@ namespace JogoTeste
             AtualizarRecursos();
         }
 
-        private void Centralizar(Panel interno, Panel pai)
-        {
-            interno.Left = (pai.Width - interno.Width) / 2;
-            interno.Top = (pai.Height - interno.Height) / 2;
-        }
-
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            Centralizar(panelCentroMenu, panelMenu);
-            Centralizar(panelCentroJogo1, panelJogo1);
-        }
-
-        private void AtualizarRecursos()
-        {
-            if (player.VidaAtual < (player.VidaMax * 0.3))
-            {
-                panelFrenteVida.BackColor = Color.Red;
-            }
-            else
-            {
-                panelFrenteVida.BackColor = Color.LimeGreen;
-            }
-
-            int larguraVida = (int)((player.VidaAtual / (float)player.VidaMax) * panelFundoVida.Width);
-            int larguraEnergia = (int)((player.EnergiaAtual / (float)player.EnergiaMax) * panelFundoEnergia.Width);
-
-            panelFrenteVida.Width = larguraVida;
-            panelFrenteEnergia.Width = larguraEnergia;
-        }
-
+        // Barra de Vida e de Energia, e suas funções
         private void panelFrenteVida_MouseEnter(object sender, EventArgs e)
         {
             labelVida.Text = $"{player.VidaAtual}/{player.VidaMax}";
@@ -96,31 +88,25 @@ namespace JogoTeste
             labelEnergia.Visible = false;
         }
 
-        private void DanoAoPlayer(int dano)
+        private void AtualizarRecursos()
         {
-            player.VidaAtual -= dano;
-            AtualizarRecursos();
+            if (player.VidaAtual < (player.VidaMax * 0.3))
+            {
+                panelFrenteVida.BackColor = Color.Red;
+            }
+            else
+            {
+                panelFrenteVida.BackColor = Color.LimeGreen;
+            }
+
+            int larguraVida = (int)((player.VidaAtual / (float)player.VidaMax) * panelFundoVida.Width);
+            int larguraEnergia = (int)((player.EnergiaAtual / (float)player.EnergiaMax) * panelFundoEnergia.Width);
+
+            panelFrenteVida.Width = larguraVida;
+            panelFrenteEnergia.Width = larguraEnergia;
         }
 
-        private void AtacarAlvo(int alvoId, int dano)
-        {
-
-        }
-
-        public class Inimigo
-        {
-            public int Vida { get; set; }
-            public string Nome { get; set; }
-        }
-
-        List<Inimigo> inimigos = new List<Inimigo>();
-
-        private void btnSkills_Click(object sender, EventArgs e)
-        {
-            panelControls.Visible = false;
-            panelSkillsMenu.Visible = true;
-        }
-
+        //Botões de Voltar para o Menu de Jogo Principal
         private void VoltarMenuControles()
         {
             panelSkillsMenu.Visible = false;
@@ -136,6 +122,18 @@ namespace JogoTeste
         private void btnVoltar2_Click(object sender, EventArgs e)
         {
             VoltarMenuControles();
+        }
+
+        private void btnVoltar3_Click(object sender, EventArgs e)
+        {
+            VoltarMenuControles();
+        }
+        
+        // Botões do Menu de Jogo Principal
+        private void btnSkills_Click(object sender, EventArgs e)
+        {
+            panelControls.Visible = false;
+            panelSkillsMenu.Visible = true;
         }
 
         private void btnItems_Click(object sender, EventArgs e)
@@ -155,9 +153,20 @@ namespace JogoTeste
 
         }
 
-        private void btnVoltar3_Click(object sender, EventArgs e)
+        // Funções de Combate
+        private void DanoAoPlayer(int dano)
         {
-            VoltarMenuControles();
+            player.VidaAtual -= dano;
+            AtualizarRecursos();
         }
+
+        private void AtacarAlvo(int alvoId, int dano)
+        {
+
+        }
+        
+
+
+
     }
 }
